b9f824a3a9a702b27bb813dd82cf1c5581c2e917
maldonadoenzo88-cell <maldonadoenzo88@gmail.com>
2026-03-27 14:16:52 -0300

    Nuevos archivos

---
+++ EJ_1_TP3/src/ej_1_tp3/EJ_1_TP3.java
@@ -0,0 +1,24 @@
+
+package ej_1_tp3;
+
+
+public class EJ_1_TP3 {
+
+    
+    public static void main(String[] args) {
+        
+        int contador = 0;
+        /*
+        while(contador < 100){
+            System.out.println("Valor de contador = " + contador);
+            //contador = contador + 1;
+            contador++;
+        }*/
+        do {            
+            System.out.println("contador = " + contador);
+            contador++;
+        } while (contador <= 100);
+        
+    }
+    
+}
